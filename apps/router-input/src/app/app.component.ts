b7c2e3a1f0b63b8874bc78c3a688b5b513012c94
Tim Herker <113173499+ti-he@users.noreply.github.com>
2023-09-03 20:44:09 +0200

    refactor(answer:22): supply and read route parameters by @Input binding
    - also put templates in separate files

--- apps/router-input/src/app/app.component.ts
+++ apps/router-input/src/app/app.component.ts
@@ -6,17 +6,7 @@ import { RouterLink, RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterModule, ReactiveFormsModule],
   selector: 'app-root',
-  template: ` <label for="userName">UserName</label>
-    <input id="userName" type="text" [formControl]="userName" />
-    <label for="testId">TestId</label>
-    <input id="testId" type="number" [formControl]="testId" />
-    <button
-      [routerLink]="'test/' + testId.value"
-      [queryParams]="{ user: userName.value }">
-      Test
-    </button>
-    <button routerLink="/">HOME</button>
-    <router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   userName = new FormControl();

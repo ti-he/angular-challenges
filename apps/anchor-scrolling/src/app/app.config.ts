a64f1b8deeaa058d41e3b2264f95027563121254
Tim Herker <113173499+ti-he@users.noreply.github.com>
2023-09-06 01:31:00 +0200

    refactor(answer:21): let angular handle routing
    - the key is the option "withInMemoryScrolling" passed to the router in app config.

--- apps/anchor-scrolling/src/app/app.config.ts
+++ apps/anchor-scrolling/src/app/app.config.ts
@@ -1,6 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  // this option is awesome, but rarely used it seems
+  providers: [
+    provideRouter(appRoutes),
+    [
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+    ],
+  ],
 };

4fc4e508e4dae4ed18ed9c1a01064c05a9fa70dd
Tim Herker <113173499+ti-he@users.noreply.github.com>
2023-09-21 22:36:13 +0200

    refactor(answer-23): move html into own file

--- apps/testing-harness/src/app/child.component.ts
+++ apps/testing-harness/src/app/child.component.ts
@@ -9,71 +9,7 @@ import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-child',
-  template: `
-    <mat-card>
-      <mat-card-content>
-        <h2 class="my-3">Slider configuration</h2>
-
-        <section class="flex items-center">
-          <mat-form-field class="m-4 max-w-[180px] w-full">
-            <mat-label>Value</mat-label>
-            <input
-              matInput
-              type="number"
-              [(ngModel)]="value"
-              id="input-value" />
-          </mat-form-field>
-          <mat-form-field class="m-4 max-w-[180px] w-full">
-            <mat-label>Min value</mat-label>
-            <input matInput type="number" [(ngModel)]="min" id="input-min" />
-          </mat-form-field>
-          <mat-form-field class="m-4 max-w-[180px] w-full">
-            <mat-label>Max value</mat-label>
-            <input matInput type="number" [(ngModel)]="max" id="input-max" />
-          </mat-form-field>
-          <mat-form-field class="m-4 max-w-[180px] w-full">
-            <mat-label>Step size</mat-label>
-            <input matInput type="number" [(ngModel)]="step" id="input-step" />
-          </mat-form-field>
-        </section>
-
-        <section class="flex items-center">
-          <mat-checkbox [(ngModel)]="showTicks">Show ticks</mat-checkbox>
-        </section>
-
-        <section class="flex items-center">
-          <mat-checkbox [(ngModel)]="thumbLabel">Show thumb label</mat-checkbox>
-        </section>
-
-        <section class="flex items-center">
-          <mat-checkbox [(ngModel)]="disabled">Disabled</mat-checkbox>
-        </section>
-      </mat-card-content>
-    </mat-card>
-
-    <mat-card class="flex">
-      <mat-card-content>
-        <div class="flex gap-10">
-          <button mat-mini-fab (click)="back()">
-            <mat-icon>arrow_back_ios</mat-icon>
-          </button>
-          <mat-slider
-            class="m-4"
-            [disabled]="disabled"
-            [max]="max"
-            [min]="min"
-            [step]="step"
-            [discrete]="thumbLabel"
-            [showTickMarks]="showTicks">
-            <input matSliderThumb [(ngModel)]="value" />
-          </mat-slider>
-          <button mat-mini-fab (click)="forward()">
-            <mat-icon>arrow_forward_ios</mat-icon>
-          </button>
-        </div>
-      </mat-card-content>
-    </mat-card>
-  `,
+  templateUrl: './child.component.html',
   styles: [
     `
       .mat-mdc-slider {
